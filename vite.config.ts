0bf18c725cc3390b3162582b072df56c0ee62e51
Romain FREYDIGER <romain.freydiger@outlook.fr>
2025-01-22 22:49:38 +0100

    chore: change vite config (took from minimal installation)

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,13 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [analog({
+    ssr: false,
+    static: true,
+    prerender: {
+      routes: [],
+    },
+  })],
   test: {
     globals: true,
     environment: 'jsdom',
